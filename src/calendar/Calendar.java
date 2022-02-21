5bb108e726f48269f673351ac994bb3e3cac2e06
bbibig <bjh84181202@gmail.com>
2022-02-21 13:54:17 +0900

    Update using variable

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,8 +1,7 @@
 package calendar;
 
 public class Calendar {
-
-	public static void main(String[] args) {
+	public void printSampleCalendar() {
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7\r\n"
--- src/calendar/Lastday.java
+++ src/calendar/Lastday.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 
 public class Lastday {
 
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
 	public static void main(String[] args) {
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
+		Lastday last = new Lastday();
 		System.out.println("달을 입력하세요.");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
-		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
+		System.out.printf("%d월은 %d일까지 있습니다.", month, last.getMaxDaysOfMonth(month));
 		scanner.close();
 	}
 }
+	
