e76c1473871587fc64b31191d23bbf1d58c5eb12
bbibig <bjh84181202@gmail.com>
2022-02-22 23:20:21 +0900

    add first weekday function

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -5,22 +5,37 @@ import java.util.Scanner;
 public class Calendar {
 	Lastday last = new Lastday();
 
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
+
+		// print blank space
+		for (int j = 0; j < weekday; j++) {
+			System.out.print("   ");
+		}
 		
 		int maxDay = last.getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
 
-		for (int i = 1; i <= maxDay; i++) {
+		// print first line
+		for (int k = 1; k <= count; k++) {
+			System.out.printf("%3d", k);
+		}
+		System.out.println();
+
+		// print second line to last
+		count++;
+		for (int i = count; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+			if (i % 7 == delim) {
 				System.out.println();
 			}
-
+			
 //		System.out.println(" 1  2  3  4  5  6  7\r\n" + " 8  9 10 11 12 13 14\r\n" + "15 16 17 18 19 20 21\r\n"
 //				+ "22 23 24 25 26 27 28\r\n" + "29 30 31");
 		}
-
+		System.out.println();
 	}
 }
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -3,8 +3,19 @@ package calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	private final static String PROMPT = "cal> ";
-
+	
+	public int firstDay(String week) {
+		if (week.equals("su")) return 0;
+		else if (week.equals("mo")) return 1;
+		else if (week.equals("tu")) return 2;
+		else if (week.equals("we")) return 3;
+		else if (week.equals("th")) return 4;
+		else if (week.equals("fr")) return 5;
+		else if (week.equals("sa")) return 6;
+		else
+			return 0;
+	}
+		
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
@@ -12,24 +23,30 @@ public class Prompt {
 
 		int month = 1;
 		int year = 1;
+		int weekday = 1;
 		
 		while (true) {			
 			System.out.println();
 			System.out.println("연도을 입력하세요.");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
+			if (year == -1)
+				break;
 			
 			System.out.println("월을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();	
 			
-		if (month == -1) {
-			break;
-		}	
-		if (month > 12) {
+			System.out.println("첫 번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
+			System.out.println("WEEKDAY> ");
+			String str_weekday = scanner.next();
+			weekday = firstDay(str_weekday);
+			
+		if (month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
 			continue;
 		}
-		cal.printCalendar(year, month);
+		cal.printCalendar(year, month, weekday);
 		}
 		System.out.println("Bye!");
 	}
